11cefd16f65b4b8db36399e2ce4db7ee3324fcff
JaxonTerrace22 <jaxonterrace@gmail.com>
2024-10-01 13:02:17 -0700

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
-using System;
-
-class Program
+#nullable disable
+public class Prep3
 {
-    static void Main(string[] args)
+    public void Run()
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Try Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Try Lower");
+            }
+            else
+            {
+                Console.WriteLine("You got it correct!");
+            }
+        } 
     }
 }
